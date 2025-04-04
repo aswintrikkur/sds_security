d532a8a0bb63ae1fa99bc46bf248ceea2157ec12
Aswinkumar C M <aswin.trikkur@gmail.com>
2025-04-04 17:27:29 +0530

    footer complete

--- client/src/layout/RootLayout.jsx
+++ client/src/layout/RootLayout.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Footer } from "../components/ui/Footer";
+import { Footer } from "../components/ui/footer/Footer";
 import { Header } from "../components/ui/Header";
 
 export const RootLayout = ({ children }) => {
